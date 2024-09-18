aa066507172a2281f5ef000b3938dba0331dc72d
matt11matthew <matt11matthew@gmail.com>
2024-09-17 21:46:47 -0400

    1

--- hw1/vm.c
+++ hw1/vm.c
@@ -47,19 +47,23 @@ void executeSyscall(bin_instr_t instruction) {
 void handleInstruction(bin_instr_t instruction, instr_type type, int i ) {
     switch (type) {
         case comp_instr_type:
+            printf("comp_instr_type");
             break;
         case other_comp_instr_type:
+            printf("other_comp_instr_type");
             break;
         case immed_instr_type:
-
+            printf("immed_instr_type");
             break;
         case jump_instr_type:
+            printf("jump_instr_type");
             break;
         case syscall_instr_type:
+            printf("syscall_instr_type");
             executeSyscall(instruction);
         return;
         case error_instr_type:
-
+            printf("error_instr_type");
             break;
     }
     if (type != syscall_instr_type) {
@@ -83,19 +87,22 @@ void handleBOFFile(char * file_name, int should_print) {
     // FP = header.stack_bottom_addr;
 
 
-    for (int i = 0; i < header.text_length; i++) { //loop header
-        if (i > MEMORY_SIZE_IN_WORDS) {
-            fprintf(stderr, "Error: Too many words in BOF file.\n");
-            bof_close(file);
-            return;
-        }
-
-        bin_instr_t instruction = instruction_read(file);
-        instr_type  type = instruction_type(instruction);
-        handleInstruction(instruction, type, i );
-
-
-    }
+    // for (int i = 0; i < header.text_length; i++) { //loop header
+    //     if (i > MEMORY_SIZE_IN_WORDS) {
+    //         fprintf(stderr, "Error: Too many words in BOF file.\n");
+    //         bof_close(file);
+    //         return;
+    //     }
+    //
+    //     bin_instr_t instruction = instruction_read(file);
+    //     instr_type  type = instruction_type(instruction);
+    //     handleInstruction(instruction, type, i );
+    //
+    //
+    // }
+    bin_instr_t instruction = instruction_read(file);
+    instr_type  type = instruction_type(instruction);
+    handleInstruction(instruction, type, 0 );
     bof_close(file);
 }
 
