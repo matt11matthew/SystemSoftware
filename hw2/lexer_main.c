eaec0d6684e4716c49d222a71e1f7a53151447df
matt11matthew <matt11matthew@gmail.com>
2024-10-11 19:03:14 -0400

    1

--- hw2/lexer_main.c
+++ hw2/lexer_main.c
@@ -14,13 +14,19 @@
 
 
 int main(int argc, char **argv) {
-
     if (argc!=2) {
         fprintf(stderr, "Usage: %s <file>\n", argv[0]); //Usage
         return 0;
     }
+
     lexer_init(argv[1]); //Init lexer
     lexer_output(); //Print lexer output
 
+    if (lexer_has_errors()){
+        exit(1);
+        return 1;
+    }
+
+    exit(0);
     return 0;
 }
