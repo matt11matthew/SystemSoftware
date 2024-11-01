a4da12471b2c212028c7be571e21d144e568aec2
matt11matthew <matt11matthew@gmail.com>
2024-11-01 11:17:40 -0400

    1

--- hw3/scope_check.c
+++ hw3/scope_check.c
@@ -5,5 +5,30 @@
 
 #include "scope_check.h"
 #include "ast.h"
+#include <stddef.h>
 void scope_check_program(block_t block) {
+   // printf("1: %s", block.file_loc->filename); //We have the file name
+
+    const_decl_t *current = block.const_decls.start;
+
+
+
+//    printf("\n");
+    while (current != NULL) {
+        const_def_list_t list = current->const_def_list;
+
+        const_def_t *start = list.start;
+        while (start != NULL) {
+            //printf("%s=%s\n",start->ident.name, start->number.text);
+            start = start->next;
+
+
+
+        }
+
+
+
+
+        current = current->next;
+    }
 }
