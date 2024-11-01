05ca383dcb4f6da59d31f1ec8ee501ebd6175527
matt11matthew <matt11matthew@gmail.com>
2024-11-01 17:07:34 -0400

    1

--- hw3/scope_check.c
+++ hw3/scope_check.c
@@ -1,34 +1,83 @@
 //
 // Created by matt1 on 10/18/2024.
 //
-
-
+#include <stdio.h>
 #include "scope_check.h"
 #include "ast.h"
+#include "scope.h"
+#include "symtab.h"
 #include <stddef.h>
+
 void scope_check_program(block_t block) {
-   // printf("1: %s", block.file_loc->filename); //We have the file name
+    symtab_enter_scope();
+    scope_check_const_decls(block.const_decls);
+    scope_check_varDecls(block.var_decls);
+    scope_check_proc_decls(block.proc_decls);
+    scope_check_stmts(block.stmts);
+    symtab_leave_scope();
+}
 
-    const_decl_t *current = block.const_decls.start;
 
+void scope_check_const_decls(const_decls_t const_decl){
+    if (const_decl.start==NULL){
+        printf("NULL const_decl\n");
+    }
+}
 
+void scope_check_assign_stmt(assign_stmt_t assignStmt) {
+    char* assignName = assignStmt.name;
 
-//    printf("\n");
+    printf("%s =", assignStmt.name);
+    id_use* idUsed = symtab_lookup(assignName);
+    if (idUsed==NULL) {
+        printf(" Unset\n");
+    } else {
+        printf(" Set\n");
+
+    }
+
+}
+/*
+ * typedef enum { assign_stmt, call_stmt, if_stmt, while_stmt,
+	       read_stmt, print_stmt, block_stmt } stmt_kind_e;
+
+ */
+void scope_check_stmt(stmt_t *stmt) {
+    if (stmt == NULL) return;
+
+    switch (stmt->stmt_kind) {
+        case assign_stmt:
+            printf("- Assign statement\n");
+            scope_check_assign_stmt(stmt->data.assign_stmt);
+            break;
+    }
+}
+
+void scope_check_stmts(stmts_t stmts) {
+    if (stmts.stmt_list.start==NULL){
+        printf("NULL stmt_list\n");
+        return;
+    }
+    printf("Statements found:\n");
+    stmt_t *current = stmts.stmt_list.start;
     while (current != NULL) {
-        const_def_list_t list = current->const_def_list;
-
-        const_def_t *start = list.start;
-        while (start != NULL) {
-            //printf("%s=%s\n",start->ident.name, start->number.text);
-            start = start->next;
-
-
-
-        }
-
-
-
+        scope_check_stmt(current);
 
         current = current->next;
+    }
+
+}
+
+
+void scope_check_proc_decls(proc_decls_t procDecls){
+    proc_decl_t *current = procDecls.proc_decls;
+//    while
+}
+
+
+void scope_check_varDecls(var_decls_t varDecls){
+    if (varDecls.var_decls == NULL ) {
+        printf("NULL var_decls\n");
+        return;
     }
 }
