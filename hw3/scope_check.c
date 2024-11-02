fa70579c389fef539b7cd3cea9f435e06bd3ff91
matt11matthew <matt11matthew@gmail.com>
2024-11-02 13:04:33 -0400

    1

--- hw3/scope_check.c
+++ hw3/scope_check.c
@@ -10,6 +10,7 @@
 #include <stddef.h>
 
 bool DEBUG = false;
+void check_ident_express(struct expr_s xp);
 
 void scope_check_program_s(struct block_s block) {
     symtab_enter_scope();
@@ -39,19 +40,16 @@ void handleAlreadyExist(char *name, file_location loc, id_kind kind) {
                          name, kind2str(id->attrs->kind));
 }
 
-
 void scope_push_constDefList(const_def_list_t identityList) {
     const_def_t *st = identityList.start;
 
-
     while (st != NULL) {
         char *name = st->ident.name;
-        if (symtab_declared_in_current_scope(name)) {
+        if (symtab_declared(name)) {
             handleAlreadyExist(name, *st->file_loc, constant_idk);
             st = st->next;
             continue;
         }
-
 
         id_attrs *attrs = create_id_attrs(*identityList.file_loc, constant_idk, 0);
 
@@ -62,67 +60,100 @@ void scope_push_constDefList(const_def_list_t identityList) {
 
 void scope_check_const_decls(const_decls_t const_decl) {
     if (const_decl.start == NULL) {
-        if (DEBUG) {
-            printf("NULL const_decl\n");
-        }
+
         return;
     }
-    //DO
+    //TODO
     const_decl_t *cur = const_decl.start;
 
     while (cur != NULL) {
-        /*
-                const_def_list_t def_list = cur->const_def_list;
-                const_def_t *def = def_list.start;
-                while(def != NULL){
-                    //TODO: declare identifiers
-                    def = def->next;
-                }
-        */
-        //        scope_check_constDefList( cur->const_def_list );
         scope_push_constDefList(cur->const_def_list);
         cur = cur->next;
     }
 }
 
-void scope_check_assign_stmt(assign_stmt_t assignStmt) {
-    char *assignName = assignStmt.name;
-    if (DEBUG) {
-    }
-    printf("(%s:%d) %s\n", assignStmt.file_loc->filename, assignStmt.file_loc->line, assignName);
-    bool idUsed = symtab_declared(assignName);
+bool check_ident(char* name, file_location loc) {
+  //printf("(%s:%d) %s\n", loc.filename, loc.line, name);
+    bool idUsed = symtab_declared(name);
+
     if (!idUsed) {
-        bail_with_prog_error(*assignStmt.file_loc,
+        bail_with_prog_error(loc,
                              "identifier \"%s\" is not declared!",
-                             assignName);
+                             name);
+        return false;
     }
+    return true;
+}
+
+void  check_binary_expr(binary_op_expr_t bin) {
+    struct expr_s* xp1 = bin.expr1;
+    if (xp1!=NULL) {
+        check_ident_express(*xp1);
+    }
+    struct expr_s* xp2 = bin.expr2;
+    if (xp2!=NULL) {
+        check_ident_express(*xp2);
+    }
+
+}
+
+void check_ident_express(struct expr_s xp) {
+
+    switch (xp.expr_kind) {
+        case expr_bin:
+             //  printf("%s:",     "expr_bin");
+            check_binary_expr(xp.data.binary);
+            break;
+        case expr_ident:
+            // printf("%s:",     "expr_ident");
+            ident_t ident = xp.data.ident;
+            check_ident(ident.name,*ident.file_loc);
+            break;
+        case expr_number:
+           // printf("%s:",     "expr_number");
+            break;
+        case expr_negated:
+          //  printf("%s:",     "expr_negated");
+            break;
+    }
+}
+
+void scope_check_assign_stmt(assign_stmt_t assignStmt) {
+   char *assignName = assignStmt.name;
+   if ( check_ident(assignName,*assignStmt.file_loc)) {
+       struct expr_s xp = *assignStmt.expr;
+       check_ident_express(xp);
+   }
 }
 
 void scope_check_if_stmt(if_stmt_t ifStmt) {
     condition_t cond = ifStmt.condition;
+    // printf("%d: Condition kind: %d\n", ifStmt.file_loc->line, cond.cond_kind);
+    // printf("else_stmts: %p, then_stmts: %p\n", (void *)ifStmt.else_stmts, (void *)ifStmt.then_stmts);
+    //
+
     if (cond.cond_kind == ck_db) {
-        printf("ck_db\n");
+
+        db_condition_t db = cond.data.db_cond;
+        check_ident_express(db.dividend);
+        check_ident_express(db.divisor);
     }
     if (cond.cond_kind == ck_rel) {
+
         rel_op_condition_t rel = cond.data.rel_op_cond;
-        if (rel.expr1.expr_kind == expr_ident) {
-            char *name = rel.expr1.data.ident.name;
-            if (!symtab_declared(name)) {
-                //identifier "d" is not declared!
-                bail_with_prog_error(*rel.expr1.file_loc,
-                                     "identifier \"%s\" is not declared!",
-                                     name);
-            }
-        }
-    }
 
-
-    if (ifStmt.else_stmts != NULL) {
-        scope_check_stmts(*ifStmt.else_stmts);
+        check_ident_express(rel.expr1);
+        check_ident_express(rel.expr2);
     }
 
     if (ifStmt.then_stmts != NULL) {
+        //printf("\n(2)\n");
         scope_check_stmts(*ifStmt.then_stmts);
+    }
+    if (ifStmt.else_stmts != NULL) {
+      //  printf("\n(1)\n");
+        scope_check_stmts(*ifStmt.else_stmts);
+
     }
 }
 
@@ -132,6 +163,7 @@ void scope_check_while_stmt(while_stmt_t while_stmt) {
     }
 }
 
+
 /*
  * typedef enum { assign_stmt, call_stmt, if_stmt, while_stmt,
 	       read_stmt, print_stmt, block_stmt } stmt_kind_e;
@@ -140,15 +172,24 @@ void scope_check_stmt(stmt_t *stmt) {
     if (stmt == NULL) return;
 
     switch (stmt->stmt_kind) {
+        case print_stmt:
+            check_ident_express(stmt->data.print_stmt.expr);
+            break;
+        case read_stmt:
+            check_ident(stmt->data.read_stmt.name,*stmt->data.read_stmt.file_loc);
+            break;
         case block_stmt:
             struct block_s *block = stmt->data.block_stmt.block;
             scope_check_program_s(*block);
             break;
         case if_stmt:
+
             scope_check_if_stmt(stmt->data.if_stmt);
             break;
         case while_stmt:
+
             scope_check_while_stmt(stmt->data.while_stmt);
+            break;
         case assign_stmt:
             scope_check_assign_stmt(stmt->data.assign_stmt);
             break;
@@ -188,8 +229,6 @@ void scope_push_identList(ident_list_t identityList) {
             continue;
         }
 
-        //        id_use curID = id_use_create();
-
         id_attrs *attrs = create_id_attrs(*identityList.file_loc, variable_idk, 0);
 
         symtab_insert(st->name, attrs);
@@ -202,9 +241,7 @@ void scope_check_identList(ident_list_t identityList, id_kind type) {
 
 void scope_check_varDecls(var_decls_t varDecls) {
     if (varDecls.var_decls == NULL) {
-        if (DEBUG) {
-            printf("NULL var_decls\n");
-        }
+
         return;
     }
 
--- hw3/test.sh
+++ hw3/test.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/bash
-echo calling make clean
+
+echo "calling make clean"
 sudo make clean
-echo calling make compiler
+
+echo "calling make compiler"
 sudo make compiler
+
 clear
-echo calling custom.spl
-sudo ./compiler custom.spl > custom.out 2>&1
+
+echo "calling custom.spl"
+if [[ "$1" == "-v" ]]; then
+  sudo valgrind ./compiler custom.spl > custom.out 2>&1
+else
+  sudo ./compiler custom.spl > custom.out 2>&1
+fi
 
 sleep 1
+
 echo "Output of custom.out:"
 echo "$(<custom.out)"
