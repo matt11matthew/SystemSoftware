9862313df3262d140402b74a82e2261e493b2a8a
matt11matthew <matt11matthew@gmail.com>
2024-11-14 13:48:43 -0500

    Update gen_code.c

--- hw4/gen_code.c
+++ hw4/gen_code.c
@@ -15,20 +15,17 @@ void gen_code_program(BOFFILE bf, block_t b) {
     // Set the magic number in the header
     bof_write_magic_to_header(&header);
 
-
+    // Set text section parameters
     header.text_start_address = 0;
-    header.text_length = 1;
-    header.data_start_address = 1024;
-    header.data_length = 1024;
-    header.stack_bottom_addr = 0;
-    // code_seq_debug_print(stdout, code_seq_singleton(code_exit(0)));
+    header.text_length = sizeof(instruction_t); // Set to the actual instruction size, e.g., sizeof(code_exit(0)->instr)
 
+    // Set data section parameters
+    header.data_start_address = header.text_start_address + header.text_length;
+    header.data_length = 1024;
+    header.stack_bottom_addr = header.data_start_address + header.data_length;
+
+    // Write header and single exit instruction
     bof_write_header(bf, header);
-    bof_write_word(bf,code_exit(0));
-    bof_write_word(bf,);
-//Reference HW1
-/*
- * assebly.c
- */
+    instruction_write_bin_instr(bf, code_exit(0)->instr);
 }
 
