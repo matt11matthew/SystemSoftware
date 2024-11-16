abab19281443ceefd09473fcec78c101fe74dbad
matt11matthew <matt11matthew@gmail.com>
2024-11-15 19:40:24 -0500

    Update gen_code.c

--- hw4/gen_code.c
+++ hw4/gen_code.c
@@ -163,7 +163,7 @@ code_seq gen_code_rel_op_condition(rel_op_condition_t cond) {
     return result;
 }
 code_seq gen_code_if_stmt(if_stmt_t stmt, code_seq base) {
-    // Generate condition code
+    // Generate the condition code
     if (stmt.condition.cond_kind == ck_rel) {
         code_seq condition_code = gen_code_rel_op_condition(stmt.condition.data.rel_op_cond);
         code_seq_concat(&base, condition_code);
@@ -173,7 +173,7 @@ code_seq gen_code_if_stmt(if_stmt_t stmt, code_seq base) {
     code_seq then_code = gen_code_stmts(*stmt.then_stmts);
     int then_size = code_seq_size(then_code);
 
-    // Generate "else" block
+    // Generate "else" block (if it exists)
     code_seq else_code = code_seq_empty();
     int else_size = 0;
     if (stmt.else_stmts != NULL) {
@@ -181,17 +181,17 @@ code_seq gen_code_if_stmt(if_stmt_t stmt, code_seq base) {
         else_size = code_seq_size(else_code);
     }
 
-    // Debug offsets
     printf("Then block size: %d\n", then_size);
     printf("Else block size: %d\n", else_size);
 
-    // Add conditional jump to skip "then" block if condition is false
+    // Add conditional jump to skip the "then" block if condition is false
+    // BLEZ should skip over "then" block + 1 instruction (unconditional jump)
     code_seq_add_to_end(&base, code_jrel(then_size + 1));
 
     // Append "then" block
     code_seq_concat(&base, then_code);
 
-    // Add unconditional jump to skip "else" block
+    // Add unconditional jump to skip the "else" block (if it exists)
     if (stmt.else_stmts != NULL) {
         code_seq_add_to_end(&base, code_jrel(else_size));
         code_seq_concat(&base, else_code);
@@ -199,9 +199,6 @@ code_seq gen_code_if_stmt(if_stmt_t stmt, code_seq base) {
 
     return base;
 }
-
-
-
 
 
 /*
